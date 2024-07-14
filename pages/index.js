dcd37f12cd858fd3045396f06fdc5645cdaa3da5
Bruno Marchese <82626253+Bmarchese@users.noreply.github.com>
2024-07-14 19:50:03 +0000

    configurando prettier

--- pages/index.js
+++ pages/index.js
@@ -1,5 +1,5 @@
-function Home(){
-    return <h1>Bora combinar de comer um burgão top? &#x1F47A;</h1>
+function Home() {
+  return <h1>Bora combinar de comer um burgão top? &#x1F47A;</h1>;
 }
 
 export default Home;
